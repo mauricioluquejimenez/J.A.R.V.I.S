e7e9ea5eaf7883afa91dbda5f2770779c4e87d73
Mauricio Luque Jiménez <maulujim.work@gmail.com>
2026-09-14 12:37:53 +0200

    chore(config): add init script [v0.3.1]
    Add simple script to activate virtual environement and launch orchestrator at once

---
+++ init.ps1
@@ -0,0 +1,5 @@
+# 1. Activa el entorno virtual en la sesión actual
+. .\.venv\Scripts\Activate.ps1
+
+# 2. Ejecuta el orquestador usando la versión de Python del entorno virtual
+.\.venv\Scripts\python.exe -m modules.core.orchestrator
